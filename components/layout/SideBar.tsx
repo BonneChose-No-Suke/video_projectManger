5b0e45006c312a6cf9bfacba3c82fe1aed45c3ff
Kosuke Takeu <n.kosuke0906@gmail.come>
2024-04-20 23:59:37 +0900

    layout sidebar 実装 #デザイン追加とページジャンプ 未:#必要ページを追加後Hover-Cardの中身実装

--- components/layout/SideBar.tsx
+++ components/layout/SideBar.tsx
@@ -1,55 +1,71 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 import { FC } from 'react';
 import {
   HiChevronRight,
-  HiUser,
   HiHome,
   HiSpeakerphone,
-  HiUsers,
+  HiUserCircle,
+  HiUserGroup,
 } from 'react-icons/hi';
+import { usePathname } from 'next/navigation';
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '../ui/hover-card';
 
 export const SideBar = () => {
   const { data: session, status } = useSession();
+
   return (
     <>
-      <div className='w-48 h-full bg-stone-100 px-3 py-6 max-[960px]:hidden'>
+      <div className='w-52 h-full bg-stone-50 pr-1 pl-3 py-6 max-[960px]:hidden'>
         <div className='ml-3 mt-2 mb-1 text-2xl font-bold flex justify-start'>
           Icon
         </div>
         <div className='flex flex-col my-3'>
-          <NavigationItem item='user' active={false} />
-          <NavigationItem item='home' active={true} />
-          <NavigationItem item='project' active={false} />
-          <NavigationItem item='team' active={false} />
+          <NavigationItem item='user' />
+          <NavigationItem item='home' />
+          <NavigationItem item='project' />
+          <NavigationItem item='team' />
         </div>
       </div>
     </>
   );
 };
 
-const NavigationItem: FC<{ item: string; active: boolean }> = ({
-  item,
-  active,
-}) => {
+const NavigationItem: FC<{ item: string }> = ({ item }) => {
+  const pathName = usePathname();
+  const isActive = pathName.split('/')[1] === getPageName(item);
   return (
-    <div className='flex justify-between items-center px-2 py-4 transition-colors duration-300 hover:bg-stone-300/30  hover:rounded-md'>
-      <NavIcon
-        item={item}
-        size={'21'}
-        color={active ? '#E3483FE6' : '#78716c'}
-      />
-      <div
-        className={`grow text-xs ${
-          active ? 'font-bold text-stone-950' : 'text-stone-700'
-        }`}>
-        {getText(item)}
-      </div>
-      {item !== 'home' && (
-        <HiChevronRight color='#78716c' style={{ marginLeft: '8px' }} />
-      )}
-    </div>
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <Link href={'/' + getPageName(item)}>
+          <div className='flex justify-between text-left items-center px-2 py-4 transition-colors duration-300 hover:bg-stone-300/30  hover:rounded-md'>
+            <NavIcon
+              item={item}
+              size={'20'}
+              color={isActive ? '#E3483FE6' : '#57534e'}
+            />
+            <div
+              className={`grow text-xs ${
+                isActive ? 'font-bold text-stone-950' : 'text-stone-700'
+              }`}>
+              {getText(item)}
+            </div>
+            {item !== 'home' && (
+              <HiChevronRight color='#78716c' style={{ marginLeft: '8px' }} />
+            )}
+          </div>
+        </Link>
+      </HoverCardTrigger>
+      <HoverCardContent className='w-80'>
+        この中身はおいおい実装します
+      </HoverCardContent>
+    </HoverCard>
   );
 };
 
@@ -61,7 +77,11 @@ const NavIcon: FC<{ item: string; size: string; color: string }> = ({
   switch (item) {
     case 'user':
       return (
-        <HiUser size={size} color={color} style={{ marginRight: '10px' }} />
+        <HiUserCircle
+          size={size}
+          color={color}
+          style={{ marginRight: '10px' }}
+        />
       );
     case 'home':
       return (
@@ -77,7 +97,11 @@ const NavIcon: FC<{ item: string; size: string; color: string }> = ({
       );
     case 'team':
       return (
-        <HiUsers size={size} color={color} style={{ marginRight: '10px' }} />
+        <HiUserGroup
+          size={size}
+          color={color}
+          style={{ marginRight: '10px' }}
+        />
       );
     default:
       null;
@@ -94,6 +118,21 @@ const getText = (item: string) => {
       return 'プロジェクト';
     case 'team':
       return 'チーム';
+    default:
+      null;
+  }
+};
+
+const getPageName = (item: string) => {
+  switch (item) {
+    case 'user':
+      return 'mypage';
+    case 'home':
+      return 'dashboard';
+    case 'project':
+      return 'projects';
+    case 'team':
+      return 'team';
     default:
       null;
   }
